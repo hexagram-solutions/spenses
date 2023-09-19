132aead92a07413d53069311eb5cb34f9d22db6e
Eric Sondergard <4650644+esond@users.noreply.github.com>
2023-09-19 09:30:31 -0600

    Set up auth in blazor app

--- src/Clients/Web/Program.cs
+++ src/Clients/Web/Program.cs
@@ -1,13 +1,23 @@
+using Hexagrams.Extensions.Configuration;
 using Spenses.Client.Web;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Fast.Components.FluentUI;
+using Spenses.Application.Common;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    options.ProviderOptions.Authority = builder.Configuration.Require(ConfigConstants.SpensesOpenIdAuthority);
+    options.ProviderOptions.ClientId = builder.Configuration.Require(ConfigConstants.SpensesOpenIdClientId);
+    options.ProviderOptions.ResponseType = "code";
+});
 
 builder.Services.AddFluentUIComponents();
 
