ee1a12fabc9bc1456fe0fb117a0f65db14b5edcf
Eric Sondergard <4650644+esond@users.noreply.github.com>
2023-10-04 16:48:35 -0600

    Add performance logging behavior for to mediatr pipeline

--- src/Application/Common/ConfigConstants.cs
+++ src/Application/Common/ConfigConstants.cs
@@ -5,6 +5,7 @@ public static class ConfigConstants
 
     public const string SpensesApiAllowedOrigins = "Spenses:Api:AllowedOrigins";
     public const string SpensesApiBaseUrl = "Spenses:Api:BaseUrl";
+    public const string SpensesLoggingLongRunningRequestThreshold = "Spenses:Logging:LongRunningRequestThreshold";
 
     public const string SpensesAppConfigurationConnectionString = "Spenses-AppConfiguration-ConnectionString";
     public const string SpensesAppConfigurationSentinel = "Spenses:AppConfiguration:Sentinel";
