758165918031bee3ca3ff0886b51f6200776c246
Eric Sondergard <4650644+esond@users.noreply.github.com>
2023-11-01 10:48:38 -0600

    Fix create home modal appearance

--- src/Clients/Web/Components/Homes/CreateHomeModal.razor.cs
+++ src/Clients/Web/Components/Homes/CreateHomeModal.razor.cs
@@ -1,22 +1,22 @@
-using BlazorState;
 using Microsoft.AspNetCore.Components;
 using Spenses.Application.Models.Homes;
 using Spenses.Client.Web.Features.Homes;
 
 namespace Spenses.Client.Web.Components.Homes;
 
-public partial class CreateHomeModal : BlazorStateComponent
+public partial class CreateHomeModal
 {
     [Inject]
-    private IModalService? ModalService { get; set; }
+    private IModalService ModalService { get; init; } = null!;
 
-    public HomeProperties Home { get; set; } = new();
+    [Parameter]
+    public HomeProperties Home { get; set; } = null!;
 
-    private Validations Validations { get; set; } = new();
+    private Validations Validations { get; set; } = null!;
 
     private Task Close()
     {
-        return ModalService!.Hide();
+        return ModalService.Hide();
     }
 
     private async Task Create()
