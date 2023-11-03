017bf6a59b4261d82d2397dfb1ec39fec79e4e74
Eric Sondergard <4650644+esond@users.noreply.github.com>
2023-11-03 14:15:26 -0600

    Add more payments to sample data

--- tools/Setup/SeedData/ExpensesSeedDataTask.cs
+++ tools/Setup/SeedData/ExpensesSeedDataTask.cs
@@ -31,7 +31,7 @@ public class ExpensesSeedDataTask : ISeedDataTask
                 var expense = new Expense
                 {
                     Description = faker.Lorem.Sentence(3),
-                    Date = DateOnly.FromDateTime(DateTime.UtcNow).AddDays(Random.Shared.Next(-10, 10)),
+                    Date = DateOnly.FromDateTime(DateTime.UtcNow).AddDays(Random.Shared.Next(-10, 0)),
                     Amount = Random.Shared.NextDecimal(5, 500, 2),
                     PaidByMember = Random.Shared.NextItem(home.Members),
                     Category = Random.Shared.NextItem(home.ExpenseCategories),
