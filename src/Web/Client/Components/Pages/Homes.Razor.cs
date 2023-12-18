4078416a81d1859043b47d46b47d4a381c96aa99
Eric Sondergard <4650644+esond@users.noreply.github.com>
2023-12-18 10:10:08 -0600

    Create a dedicated component for registering UI service components

--- src/Web/Client/Components/Pages/Homes.Razor.cs
+++ src/Web/Client/Components/Pages/Homes.Razor.cs
@@ -6,25 +6,16 @@ namespace Spenses.Web.Client.Components.Pages;
 
 public partial class Homes
 {
-    //[Inject]
-    //private IHomesApi HomesApi { get; set; } = null!;
-
     [Inject]
     private IState<HomesState> HomesState { get; set; } = null!;
 
     [Inject]
     private IDispatcher Dispatcher { get; set; } = null!;
 
-    //private Application.Models.Homes.Home[] HomesItems { get; set; } = [];
-
     protected override void OnInitialized()
     {
         base.OnInitialized();
 
         Dispatcher.Dispatch(new HomesRequestedAction());
-
-        //var response = await HomesApi.GetHomes();
-
-        //HomesItems = response.Content!.ToArray();
     }
 }
