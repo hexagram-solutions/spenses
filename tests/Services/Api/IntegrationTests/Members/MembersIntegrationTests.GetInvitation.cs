958cbf4dcfa26f1fbca94f1c951aa6c26afb4c2e
Eric Sondergard <4650644+esond@users.noreply.github.com>
2024-01-29 11:42:44 -0600

    Add integration tests for fetching user invitations

--- tests/Services/Api/IntegrationTests/Members/MembersIntegrationTests.GetInvitation.cs
+++ tests/Services/Api/IntegrationTests/Members/MembersIntegrationTests.GetInvitation.cs
@@ -1,3 +1,6 @@
+using System.Net;
+using Spenses.Shared.Models.Invitations;
+
 namespace Spenses.Api.IntegrationTests.Members;
 
 public partial class MembersIntegrationTests
@@ -5,6 +8,25 @@ public partial class MembersIntegrationTests
     [Fact]
     public async Task Get_invitation_with_invalid_parameters_yields_not_found()
     {
-        throw new NotImplementedException();
+        var response = await _members.GetMemberInvitation(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid());
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+
+        var homeId = (await _homes.GetHomes()).Content!.First().Id;
+
+        response = await _members.GetMemberInvitation(homeId, Guid.NewGuid(), Guid.NewGuid());
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+
+        var member = (await _members.GetMembers(homeId)).Content!.First(m => m.User == null);
+
+        var invitationProperties = new InvitationProperties { Email = "foo@bar.com" };
+
+        var invitationResponse = await _members.PostMemberInvitation(homeId, member.Id, invitationProperties);
+
+        response = await _members.GetMemberInvitation(homeId, member.Id, Guid.NewGuid());
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+
+        response = await _members.GetMemberInvitation(homeId, member.Id, invitationResponse.Content!.Id);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        response.Content.Should().BeEquivalentTo(invitationProperties);
     }
 }
--- tests/Services/Api/IntegrationTests/Members/MembersIntegrationTests.GetInvitations.cs
+++ tests/Services/Api/IntegrationTests/Members/MembersIntegrationTests.GetInvitations.cs
@@ -1,3 +1,5 @@
+using System.Net;
+
 namespace Spenses.Api.IntegrationTests.Members;
 
 public partial class MembersIntegrationTests
@@ -5,6 +7,17 @@ public partial class MembersIntegrationTests
     [Fact]
     public async Task Get_invitations_with_invalid_parameters_yields_not_found()
     {
-        throw new NotImplementedException();
+        var response = await _members.GetMemberInvitations(Guid.NewGuid(), Guid.NewGuid());
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+
+        var homeId = (await _homes.GetHomes()).Content!.First().Id;
+
+        response = await _members.GetMemberInvitations(homeId, Guid.NewGuid());
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+
+        var memberId = (await _members.GetMembers(homeId)).Content!.First().Id;
+
+        response = await _members.GetMemberInvitations(homeId, memberId);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
     }
 }
